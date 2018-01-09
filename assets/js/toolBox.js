21e8b6926871e6a77c04a4b6aed185e9ba46d897
Quinn Walker-Phelan <qnnwp@uw.edu>
2018-01-08 16:36:02 -0800

    Css update

--- assets/js/toolBox.js
+++ assets/js/toolBox.js
@@ -4464,16 +4464,16 @@
                     'href': 'https://cdn.rawgit.com/cirept/QA_Toolbox/master/resources/jquery-ui-1.12.1.custom/jquery-ui.min.css',
                     'rel': 'stylesheet',
                 }),
-                /*
+                
                 '$toolStyles': jQuery('<link>').attr({
                     'id': 'toolStyles',
                     'href': 'https://rawgit.com/cirept/QA_Toolbox/' + GM_info.script.version + '/assets/css/toolbox.css', // eslint-disable-line camelcase
                     'rel': 'stylesheet',
                     'type': 'text/css',
-                }),*/
+                }),
                 '$toolStyles': jQuery('<link>').attr({
                     'id': 'mycss',
-                    'href': 'https://rawgit.com/cirept/QA_Toolbox/v1.92/assets/css/toolbox.css', // eslint-disable-line camelcase
+                    'href': 'https://rawgit.com/cirept/QA_Toolbox/v1.93/assets/css/toolbox.css', // eslint-disable-line camelcase
                     'rel': 'stylesheet',
                     'type': 'text/css',
                 }),
@@ -4491,7 +4491,7 @@
                 .append(main.config.$toolboxStyles)
                 .append(main.config.$myFont)
                 .append(main.config.$jQueryUIcss)
-                /*.append(main.config.$toolStyles)*/
+                .append(main.config.$toolStyles)
                 .append(main.config.$mycss)                
                 .append(main.config.$fontAw)
                 .append(main.config.$animate);
