d876c734c6ea29da1a5d651bc47d9e11add94cf2
Quinn Walker-Phelan <qnnwp@uw.edu>
2018-01-10 15:57:49 -0800

    debug

--- assets/js/toolBox.js
+++ assets/js/toolBox.js
@@ -3212,7 +3212,7 @@
                 $currentLink.attr('target') === 'custom') {
                     $linkOverlay.addClass('opensWindow');
                 }
-                var href = $currentLink.attr('href');
+                var href = jQuery($currentLink).attr('href');
                 /*var currentURL = jQuery.trim(jQuery($currentLink).attr('href'));
                 if (currentURL.indexOf('www') > -1 || currentURL.indexOf('://') > -1) { // test for absolute path URLs
                     $linkOverlay.addClass('otherDomain');
