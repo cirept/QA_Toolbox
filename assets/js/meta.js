93603aaeafd5d20b2ee1c2f40196cd12f46d4b28
Eric Tanaka <cirept@users.noreply.github.com>
2018-03-27 13:55:00 -0700

    Update meta.js
    updated file to .user.js

--- assets/js/meta.js
+++ assets/js/meta.js
@@ -5,7 +5,7 @@
 // @author Eric Tanaka
 // @include http:*
 // @include https:*
-// @downloadURL https://raw.githubusercontent.com/cirept/QA_Toolbox/master/assets/js/meta.js
+// @downloadURL https://raw.githubusercontent.com/cirept/QA_Toolbox/master/assets/js/meta.user.js
 // @require https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
 // @require https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
 // @require https://cdn.rawgit.com/cirept/Typo.js/master/typo/typo.js
