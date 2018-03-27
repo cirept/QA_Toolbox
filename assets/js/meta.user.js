8debf0ef09a0b57909b9c9ea33b41bb8b039e6aa
Eric Tanaka <cirept@users.noreply.github.com>
2018-03-27 13:54:37 -0700

    Create meta.user.js
    this is specifically for tampermonkey.  Makes it easier to install the script

---
+++ assets/js/meta.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name QA Toolbox 3.3.1.4
+// @namespace www.cobaltgroup.com/
+// @version 3.3.1.4
+// @author Eric Tanaka
+// @include http:*
+// @include https:*
+// @downloadURL https://raw.githubusercontent.com/cirept/QA_Toolbox/master/assets/js/meta.user.js
+// @require https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
+// @require https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
+// @require https://cdn.rawgit.com/cirept/Typo.js/master/typo/typo.js
+// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.1.4-spellcheck-update/assets/js/toolBox.js
+// @run-at document-end
+// @grant GM_openInTab
+// @grant GM_setClipboard
+// @grant unsafeWindow
+// @grant GM_setValue
+// @grant GM_getValue
+// @grant GM_info
+// @grant GM_listValues
+// @grant GM_addStyle
+// @grant resource
+// @noframes
+// ==/UserScript==
