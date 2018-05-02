990087ba504471e19b641fb1ef89c0d980439d08
Eric Tanaka <cirept612@gmail.com>
2018-05-02 10:30:54 -0700

    beta11 update

--- assets/js/meta.js
+++ assets/js/meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name QA Toolbox 3.3.2-beta10
+// @name QA Toolbox 3.3.2-beta11
 // @namespace www.cobaltgroup.com/
-// @version 3.3.2-beta10
+// @version 3.3.2-beta11
 // @author Eric Tanaka
 // @include http:*
 // @include https:*
@@ -9,8 +9,8 @@
 // @require https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
 // @require https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
 // @require https://cdn.rawgit.com/cirept/Typo.js/master/typo/typo.js
-// @require https://rawgit.com/cirept/QA_Toolbox/3.3.2-beta10/assets/js/main.js
-// @resource toolStyles https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta10/assets/css/toolbox.css
+// @require https://rawgit.com/cirept/QA_Toolbox/3.3.2-beta11/assets/js/main.js
+// @resource toolStyles https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta11/assets/css/toolbox.css
 // @run-at document-end
 // @grant GM_openInTab
 // @grant GM_setClipboard
