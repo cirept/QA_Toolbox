c1537c82f838dbf9bc4f49f2bbd4542a710f2cc2
Eric Tanaka <cirept612@gmail.com>
2018-05-02 14:11:26 -0700

    beta13

--- assets/js/meta.user.js
+++ assets/js/meta.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name QA Toolbox 3.3.2-beta12
+// @name QA Toolbox 3.3.2-beta13
 // @namespace www.cobaltgroup.com/
-// @version 3.3.2-beta12
+// @version 3.3.2-beta13
 // @author Eric Tanaka
 // @include http:*
 // @include https:*
@@ -9,31 +9,31 @@
 // @require https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
 // @require https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
 // @require https://cdn.rawgit.com/cirept/Typo.js/master/typo/typo.js
-// @resource toolStyles https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta12/assets/css/toolbox.css
-// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta12/assets/js/dependencies/shared.js
-// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta12/assets/js/dependencies/qaToolbox.js
-// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta12/assets/js/dependencies/pageInformation.js
-// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta12/assets/js/dependencies/dealerName.js
-// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta12/assets/js/dependencies/webId.js
-// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta12/assets/js/dependencies/pageName.js
-// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta12/assets/js/dependencies/hTags.js
-// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta12/assets/js/dependencies/qaTools.js
-// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta12/assets/js/dependencies/imageChecker.js
-// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta12/assets/js/dependencies/linkChecker.js
-// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta12/assets/js/dependencies/spellCheck.js
-// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta12/assets/js/dependencies/speedTestPage.js
-// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta12/assets/js/dependencies/checkLinks.js
-// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta12/assets/js/dependencies/otherTools.js
-// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta12/assets/js/dependencies/showNavigation.js
-// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta12/assets/js/dependencies/seoSimplify.js
-// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta12/assets/js/dependencies/toggles.js
-// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta12/assets/js/dependencies/refreshPage.js
-// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta12/assets/js/dependencies/previewBarToggle.js
-// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta12/assets/js/dependencies/urlModifiers.js
-// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta12/assets/js/dependencies/nextGenToggle.js
-// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta12/assets/js/dependencies/m4Check.js
-// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta12/assets/js/dependencies/dynamicDisplay.js
-// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta12/assets/js/dependencies/init.js
+// @resource toolStyles https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta13/assets/css/toolbox.css
+// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta13/assets/js/dependencies/shared.js
+// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta13/assets/js/dependencies/qaToolbox.js
+// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta13/assets/js/dependencies/pageInformation.js
+// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta13/assets/js/dependencies/dealerName.js
+// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta13/assets/js/dependencies/webId.js
+// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta13/assets/js/dependencies/pageName.js
+// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta13/assets/js/dependencies/hTags.js
+// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta13/assets/js/dependencies/qaTools.js
+// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta13/assets/js/dependencies/imageChecker.js
+// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta13/assets/js/dependencies/linkChecker.js
+// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta13/assets/js/dependencies/spellCheck.js
+// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta13/assets/js/dependencies/speedTestPage.js
+// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta13/assets/js/dependencies/checkLinks.js
+// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta13/assets/js/dependencies/otherTools.js
+// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta13/assets/js/dependencies/showNavigation.js
+// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta13/assets/js/dependencies/seoSimplify.js
+// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta13/assets/js/dependencies/toggles.js
+// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta13/assets/js/dependencies/refreshPage.js
+// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta13/assets/js/dependencies/previewBarToggle.js
+// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta13/assets/js/dependencies/urlModifiers.js
+// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta13/assets/js/dependencies/nextGenToggle.js
+// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta13/assets/js/dependencies/m4Check.js
+// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta13/assets/js/dependencies/dynamicDisplay.js
+// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.2-beta13/assets/js/dependencies/init.js
 // @run-at document-end
 // @grant GM_openInTab
 // @grant GM_setClipboard
