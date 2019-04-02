0fe1f00414a95d51992341d7ab91d6491683a4e0
Eric Tanaka <cirept612@gmail.com>
2019-04-02 11:50:01 -0700

    Work : updated version
    List Changes Made:
    -

--- assets/js/preRelease.user.js
+++ assets/js/preRelease.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name QA Toolbox - PreRelease
 // @namespace www.cobaltgroup.com/
-// @version 3.5.3.4
+// @version 3.5.3.5
 // @author Eric Tanaka
 // @include http:*
 // @include https:*
