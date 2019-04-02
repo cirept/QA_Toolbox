a410daf3e3994302b5608e571cc7314695e01cf0
Eric Tanaka <cirept612@gmail.com>
2019-04-02 12:54:50 -0700

    Work : updating version for next release
    List Changes Made:
    -

--- assets/js/meta.js
+++ assets/js/meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name QA Toolbox 3.5.3.6
+// @name QA Toolbox 3.5.3.7
 // @namespace www.cobaltgroup.com/
-// @version 3.5.3.6
+// @version 3.5.3.7
 // @author Eric Tanaka
 // @include http:*
 // @include https:*
@@ -10,41 +10,41 @@
 // @downloadURL https://raw.githubusercontent.com/cirept/QA_Toolbox/master/assets/js/meta.user.js
 // @connect assets-cdk.com
 // @connect maps.googleapis.com
-// @resource toolStyles https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/css/toolbox.css
-// @resource changeLog https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/docs/CHANGELOG.md
+// @resource toolStyles https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/css/toolbox.css
+// @resource changeLog https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/docs/CHANGELOG.md
 // @require https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
 // @require https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
 // @require https://cdn.rawgit.com/cirept/Typo.js/master/typo/typo.js
 // @require https://cdn.rawgit.com/showdownjs/showdown/1.8.6/dist/showdown.min.js
-// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/js/dependencies/pageInformationPanel/pageInformation.js
-// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/js/dependencies/pageInformationPanel/dealerName.js
-// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/js/dependencies/pageInformationPanel/liveSiteURL.js
-// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/js/dependencies/pageInformationPanel/webId.js
-// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/js/dependencies/pageInformationPanel/pageName.js
-// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/js/dependencies/pageInformationPanel/hTags.js
-// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/js/dependencies/qaToolsPanel/qaTools.js
-// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/js/dependencies/qaToolsPanel/imageChecker.js
-// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/js/dependencies/qaToolsPanel/linkChecker.js
-// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/js/dependencies/qaToolsPanel/spellCheck.js
-// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/js/dependencies/qaToolsPanel/bannedWords.js
-// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/js/dependencies/qaToolsPanel/speedTestPage.js
-// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/js/dependencies/qaToolsPanel/checkLinks.js
-// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/js/dependencies/otherToolsPanel/otherTools.js
-// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/js/dependencies/otherToolsPanel/showNavigation.js
-// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/js/dependencies/otherToolsPanel/seoSimplify.js
-// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/js/dependencies/otherToolsPanel/widgetOutlines.js
-// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/js/dependencies/otherToolsPanel/viewMobile.js
-// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/js/dependencies/togglesPanel/toggles.js
-// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/js/dependencies/togglesPanel/refreshPage.js
-// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/js/dependencies/togglesPanel/previewBarToggle.js
-// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/js/dependencies/urlModPanel/urlModifiers.js
-// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/js/dependencies/urlModPanel/nextGenToggle.js
-// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/js/dependencies/urlModPanel/m4Check.js
-// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/js/dependencies/urlModPanel/autofillToggle.js
-// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/js/dependencies/dynamicDisplayPanel/dynamicDisplay.js
-// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/js/dependencies/base/shared.js
-// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/js/dependencies/base/main.js
-// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.6/assets/js/dependencies/base/qaToolbox.js
+// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/js/dependencies/pageInformationPanel/pageInformation.js
+// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/js/dependencies/pageInformationPanel/dealerName.js
+// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/js/dependencies/pageInformationPanel/liveSiteURL.js
+// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/js/dependencies/pageInformationPanel/webId.js
+// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/js/dependencies/pageInformationPanel/pageName.js
+// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/js/dependencies/pageInformationPanel/hTags.js
+// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/js/dependencies/qaToolsPanel/qaTools.js
+// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/js/dependencies/qaToolsPanel/imageChecker.js
+// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/js/dependencies/qaToolsPanel/linkChecker.js
+// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/js/dependencies/qaToolsPanel/spellCheck.js
+// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/js/dependencies/qaToolsPanel/bannedWords.js
+// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/js/dependencies/qaToolsPanel/speedTestPage.js
+// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/js/dependencies/qaToolsPanel/checkLinks.js
+// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/js/dependencies/otherToolsPanel/otherTools.js
+// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/js/dependencies/otherToolsPanel/showNavigation.js
+// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/js/dependencies/otherToolsPanel/seoSimplify.js
+// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/js/dependencies/otherToolsPanel/widgetOutlines.js
+// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/js/dependencies/otherToolsPanel/viewMobile.js
+// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/js/dependencies/togglesPanel/toggles.js
+// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/js/dependencies/togglesPanel/refreshPage.js
+// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/js/dependencies/togglesPanel/previewBarToggle.js
+// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/js/dependencies/urlModPanel/urlModifiers.js
+// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/js/dependencies/urlModPanel/nextGenToggle.js
+// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/js/dependencies/urlModPanel/m4Check.js
+// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/js/dependencies/urlModPanel/autofillToggle.js
+// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/js/dependencies/dynamicDisplayPanel/dynamicDisplay.js
+// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/js/dependencies/base/shared.js
+// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/js/dependencies/base/main.js
+// @require https://github.com/cirept/QA_Toolbox/raw/3.5.3.7/assets/js/dependencies/base/qaToolbox.js
 // @run-at document-idle
 // @grant GM_openInTab
 // @grant GM_setClipboard
